fc1e1ffd03b3a51bbd5ecf582162098ce51de1c2
mexu <mexu@seas.upenn.edu>
2015-11-07 12:18:59 -0500

    d

--- src/leet/WordSearch.java
+++ src/leet/WordSearch.java
@@ -45,6 +45,18 @@ public class WordSearch {
             if(node.end==true) return true;
             else return false;
         }
+        
+        public boolean startWith(String words){
+        	TrieNode node= this.root;
+            for(char c:words.toCharArray()){
+                if(node.children[c-'a']==null){
+                	return false;
+                }else{
+                    node=node.children[c-'a'];
+                }
+            }
+            return true;
+        }
     }
     
     public List<String> findWords(char[][] board, String[] words) {
@@ -60,13 +72,34 @@ public class WordSearch {
         for(int row=0;row<board.length;row++){
         	for(int col=0;col<board[0].length;col++){
         		
-        		dfs(board,trietree,str,visited,row,col);
+        		dfs(res,board,trietree,str,visited,row,col);
         	}
         }
         return res;
     }
     
-    private boolean dfs(char[][] board, Trie trietree, StringBuilder str,boolean[][] visited, int row, int col){
-    	return false;
+    private boolean inboundry(int rlen,int clen,int rol, int col){
+    	if(rol<rlen && rol>=0 && col<clen && col>=0) return true;
+    	else return false;
+    }
+    
+    private void dfs(List<String> res, char[][] board, Trie trietree, StringBuilder str,boolean[][] visited, int row, int col){
+    	//debug index range boundry!
+    	if(trietree.search(str.toString())){
+    		res.add(str.toString());
+    	}
+    	
+    	if(inboundry(board.length,board[0].length,row,col) && !visited[row][col] && trietree.startWith(str.toString())){
+    		visited[row][col]=true;
+    		str.append(board[row][col]);
+    		dfs(res,board,trietree,str,visited,row+1,col);
+    		dfs(res,board,trietree,str,visited,row-1,col);
+    		dfs(res,board,trietree,str,visited,row,col+1);
+    		dfs(res,board,trietree,str,visited,row,col-1);
+    		visited[row][col]=false;
+    		str.deleteCharAt(str.length()-1);
+    	}
+    	
+    	return;
     }
 }
