1b60980a299f0316e3bda95f5dd6175dabdeb103
mexu <mexu@seas.upenn.edu>
2015-11-06 23:45:09 -0500

    mergesort

---
+++ src/leet/MergeKLst.java
@@ -0,0 +1,41 @@
+package leet;
+
+public class MergeKLst {
+	public ListNode mergeKLists(ListNode[] lists) {
+        int len=lists.length-1;
+        if(lists.length==0||lists==null) return null;
+        return divideLst(lists,0,len);
+    }
+    
+    public ListNode divideLst(ListNode[] lists,int start, int end){
+        if(start<end){
+            ListNode node1=divideLst(lists,start,(start+end)/2);
+            ListNode node2=divideLst(lists,(start+end)/2+1,end);
+            return merge(node1,node2);
+        }
+        return lists[start];
+    }
+    
+    public ListNode merge(ListNode node1, ListNode node2){
+        ListNode start=new ListNode(-3);
+        ListNode pre=start;
+        
+        while(node1!=null&&node2!=null){
+            if(node1.val<node2.val){
+                start.next=node1;
+                start=node1;
+                node1=node1.next;
+            }else{
+                start.next=node2;
+                
+                start=node2;
+                node2=node2.next;
+            }
+        }
+        
+        if(node1!=null) start.next=node1;
+        else start.next=node2;
+        
+        return pre.next;
+    }
+}
