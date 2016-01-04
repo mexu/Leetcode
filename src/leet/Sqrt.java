346f6af0954c68e455ca86fcf89b079ab34578ca
mexu <mexu@seas.upenn.edu>
2016-01-04 18:35:45 -0500

    d

--- src/leet/Sqrt.java
+++ src/leet/Sqrt.java
@@ -1,10 +1,21 @@
 package leet;
 
 public class Sqrt {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	
+	public int mySqrt(int x) {
+        if(x==0) return 0;
+        //can not divide by zero
+        int low=1;
+        //start from 1
+        int high=x;
+        while(low<high){
+            int mid=(low+high)/2;
+            if(mid<=x/mid && mid+1>x/(mid+1)) return mid;
+            //this is how we check if mid is square root of x, mind <=
+            else if(mid>x/mid) high=mid;
+            else low=mid+1;
+        }
+        return low;
+    }
 
 }
